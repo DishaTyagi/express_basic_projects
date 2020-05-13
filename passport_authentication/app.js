dabc8f0274875aab43d772b15fa8d6f7ed3b535e
DishaTyagi <omgdisha@gmail.com>
2020-05-14 02:47:44 +0530

    updated app

--- passport_authentication/app.js
+++ passport_authentication/app.js
@@ -9,6 +9,7 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(express.json());    //recognizes incoming request as JSON object
 app.use(express.urlencoded({extended: true}));
+app.use(express.static('public'));
 
 app.use(
     session({
