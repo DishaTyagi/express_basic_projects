89cdf5a2f10ac7fdefed6ebd8f97fb047c88cf46
DishaTyagi <omgdisha@gmail.com>
2020-05-18 02:54:27 +0530

    proper local authentication with user profile

---
+++ node_authentication2/app.js
@@ -0,0 +1,35 @@
+var express  = require('express');
+var app  = express();
+var port = process.env.PORT || 3000;
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash = require('connect-flash');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url); // connect to our database
+
+require('./config/passport')(passport); // pass passport for configuration
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(express.urlencoded({extended: true})); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+// required for passport
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash());
+
+require('./app/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+app.listen(port, () => {
+    console.log("Server running on port 3000");
+    
+})
